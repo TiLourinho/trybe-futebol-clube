3c2e2da37726a36b3fa82cbdedff41e895c5dd6f
TiLourinho <lourinho.tiago@gmail.com>
2022-07-07 00:44:19 -0300

    feat: User model

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,37 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  public id!: number;
+  public userName!: string;
+  public role!: string;
+  public email!: string;
+  public password!: string;
+}
+
+User.init({
+  id: {
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  userName: {
+    type: DataTypes.STRING,
+  },
+  role: {
+    type: DataTypes.STRING,
+  },
+  email: {
+    type: DataTypes.STRING,
+  },
+  password: {
+    type: DataTypes.STRING,
+  },
+}, {
+  modelName: 'Users',
+  sequelize: db,
+  timestamps: false,
+  underscored: true,
+});
+
+export default User;
