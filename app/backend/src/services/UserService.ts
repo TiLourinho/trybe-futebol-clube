a703024a2bec0cd8a9042f3ba3266a93863ad5e9
TiLourinho <lourinho.tiago@gmail.com>
2022-07-08 14:18:29 -0300

    feat: user service login method

---
+++ app/backend/src/services/UserService.ts
@@ -0,0 +1,29 @@
+import * as bcrypt from 'bcryptjs';
+import { IUser, IUserM, IUserS } from '../protocols';
+import generateJWT from '../utils/generateJWT';
+
+class UserService implements IUserS {
+  constructor(private model: IUserM) {
+    this.model = model;
+  }
+
+  async login(data: IUser): Promise<string> {
+    const user = await this.model.checkUser({
+      where: { email: data.email },
+    });
+    console.log('service', user);
+
+    if (!user) throw new Error('User doesn\'t exist');
+
+    const checkPassword = await bcrypt.compare(data.password as string, user.password as string);
+
+    if (!checkPassword) throw new Error('Invalid password');
+
+    const payload = { username: user.username, email: user.email };
+    const token = generateJWT(payload);
+
+    return token;
+  }
+}
+
+export default UserService;
