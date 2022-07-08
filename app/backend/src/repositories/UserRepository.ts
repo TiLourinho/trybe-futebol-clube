18a995c8940046177dc258e450f1512eae15050b
TiLourinho <lourinho.tiago@gmail.com>
2022-07-08 14:17:50 -0300

    feat: user repository checkUser method

---
+++ app/backend/src/repositories/UserRepository.ts
@@ -0,0 +1,16 @@
+import Model from '../database/models/User';
+import { IUser, IUserM } from '../protocols/index';
+
+class UserRepository implements IUserM {
+  constructor(private model = Model) {
+    this.model = model;
+  }
+
+  async checkUser(data: object): Promise<IUser | null> {
+    const user = await this.model.findOne(data);
+
+    return user as IUser;
+  }
+}
+
+export default UserRepository;
