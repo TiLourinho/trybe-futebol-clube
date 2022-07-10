f7a4298c5e85884caf51e022039148b5c90c9886
TiLourinho <lourinho.tiago@gmail.com>
2022-07-10 15:07:31 -0300

    test: validate method

--- app/backend/src/tests/user.test.ts
+++ app/backend/src/tests/user.test.ts
@@ -3,38 +3,57 @@ import chaiHttp = require('chai-http');
 import * as sinon from 'sinon';
 import * as chai from 'chai';
 import { app } from '../app';
-import { Response } from 'superagent';
 import User from '../database/models/User';
 
 chai.use(chaiHttp);
 const { expect } = chai;
 
+const userDB = {
+  id: 1,
+  email: 'user@user.com',
+  role: 'user',
+  username: 'User',
+  password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO'
+} as User;
+
+const body = { 
+  email: 'user@user.com',
+  password: 'secret_user'
+};
+
 describe('1 - User', () => {
-  const userDB = {
-    id: 1,
-    email: 'user@user.com',
-    role: 'user',
-    username: 'User',
-    password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO'
-  } as User;
+  describe('login method', () => {
+    before(() => {
+      sinon.stub(User, 'findOne').resolves(userDB);
+    });
   
-  const body = { 
-    email: 'user@user.com',
-    password: 'secret_user'
-  };
-
-  before(() => {
-    sinon.stub(User, 'findOne').resolves(userDB);
+    after(() => {
+      (User.findOne as sinon.SinonStub).restore();
+    });
+  
+    it('tests if "login" has status 200 when succeeded', async () => {
+      const response = await chai.request(app).post('/login').send(body);
+  
+      expect(response.status).to.be.equal(200);
+      expect(response.body).to.have.a.key('token');
+    });
   });
 
-  after(() => {
-    (User.findOne as sinon.SinonStub).restore();
-  });
-
-  it('tests if findOne method has status 200 when succeeded', async () => {
-    const response = await chai.request(app).post('/login').send(body);
-
-    expect(response.status).to.be.equal(200);
-    expect(response.body).to.have.a.key('token');
+  describe('validate method', () => { 
+    before(() => {
+      sinon.stub(User, 'findByPk').resolves(userDB);
+    });
+  
+    after(() => {
+      (User.findByPk as sinon.SinonStub).restore();
+    });
+  
+    it('tests if "validate" has status 200 when succeeded', async () => {
+      const response = await chai.request(app).get('/login/validate')
+        .set('authorization', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwidXNlcm5hbWUiOiJVc2VyIiwicm9sZSI6InVzZXIiLCJlbWFpbCI6InVzZXJAdXNlci5jb20iLCJpYXQiOjE2NTc0NzU5NTB9.CtUPSJJvxw-w_KCncuuMcw9kqnYz-ESIGSBhI26_RKg');
+  
+      expect(response.status).to.be.equal(200);
+      expect(response.body).to.have.a.key('role');
+    });
   });
 });
