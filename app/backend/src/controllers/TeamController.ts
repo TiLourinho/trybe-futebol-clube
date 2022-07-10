e9b9bc033329a2f9548905e0099c08114d30c56f
TiLourinho <lourinho.tiago@gmail.com>
2022-07-10 17:02:30 -0300

    feat: team controller getById method

--- app/backend/src/controllers/TeamController.ts
+++ app/backend/src/controllers/TeamController.ts
@@ -7,11 +7,22 @@ class TeamController {
     this.service = service;
   }
 
-  async getAll(req: Request, res: Response, next: NextFunction) {
+  async getAll(_req: Request, res: Response, next: NextFunction) {
     try {
       const teams = await this.service.getAll();
 
       return res.status(StatusCodes.OK).json(teams);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async getById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = req.params;
+      const team = await this.service.getById(Number(id));
+
+      return res.status(StatusCodes.OK).json(team);
     } catch (err) {
       next(err);
     }
