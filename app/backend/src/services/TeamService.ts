0db4da3ba7b86094e3de059449bdda9d7581e81b
TiLourinho <lourinho.tiago@gmail.com>
2022-07-10 17:01:47 -0300

    feat: team service getById method

--- app/backend/src/services/TeamService.ts
+++ app/backend/src/services/TeamService.ts
@@ -1,14 +1,20 @@
-import { ITeamM, ITeamS } from '../protocols';
+import { ITeam, ITeamM, ITeamS } from '../protocols';
 
 class TeamService implements ITeamS {
   constructor(private model: ITeamM) {
     this.model = model;
   }
 
-  async getAll(): Promise<object[]> {
+  async getAll(): Promise<ITeam[]> {
     const teams = await this.model.getAll();
 
-    return teams;
+    return teams as ITeam[];
+  }
+
+  async getById(id: number): Promise<ITeam> {
+    const team = await this.model.getById(id);
+
+    return team as ITeam;
   }
 }
 
