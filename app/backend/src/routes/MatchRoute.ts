e3a6125d107805bc8c00255d75ac35e4009d5a17
TiLourinho <lourinho.tiago@gmail.com>
2022-07-11 20:54:27 -0300

    feat: endpoint PATCH /matches/:id/finish

--- app/backend/src/routes/MatchRoute.ts
+++ app/backend/src/routes/MatchRoute.ts
@@ -11,4 +11,8 @@ matchRouter.post('/', (req, res, next) => {
   MatchFactory().create(req, res, next);
 });
 
+matchRouter.patch('/:id/finish', (req, res, next) => {
+  MatchFactory().update(req, res, next);
+});
+
 export default matchRouter;
