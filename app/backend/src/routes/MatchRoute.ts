db6f6b40ae8f47342eb656eb8c72cf80861182ba
TiLourinho <lourinho.tiago@gmail.com>
2022-07-11 18:39:13 -0300

    feat: endpoint POST /matches

--- app/backend/src/routes/MatchRoute.ts
+++ app/backend/src/routes/MatchRoute.ts
@@ -7,4 +7,8 @@ matchRouter.get('/', (req, res, next) => {
   MatchFactory().getAll(req, res, next);
 });
 
+matchRouter.post('/', (req, res, next) => {
+  MatchFactory().create(req, res, next);
+});
+
 export default matchRouter;
