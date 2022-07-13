32071bdb3a062a4eadc395c79a755093c3602bbe
TiLourinho <lourinho.tiago@gmail.com>
2022-07-13 20:33:42 -0300

    feat: endpoint GET /leaderboard/away

--- app/backend/src/routes/LeaderboardRoute.ts
+++ app/backend/src/routes/LeaderboardRoute.ts
@@ -4,7 +4,11 @@ import { LeaderboardFactory } from '../utils/factory';
 const leaderboardRouter: Router = Router();
 
 leaderboardRouter.get('/home', (req, res, next) => {
-  LeaderboardFactory().getAll(req, res, next);
+  LeaderboardFactory().getAllHome(req, res, next);
+});
+
+leaderboardRouter.get('/away', (req, res, next) => {
+  LeaderboardFactory().getAllAway(req, res, next);
 });
 
 export default leaderboardRouter;
